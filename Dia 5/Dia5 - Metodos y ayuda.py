86fbe5f7ae8daf2877c7a5a98be16f0d0ee1bb15
Raul Martos <raul@MacBook-Pro-de-Raul.local>
2025-03-18 17:25:43 -0600

    Adding two more files, first functions created on python

--- Dia 5/Dia5 - Metodos y ayuda.py
+++ Dia 5/Dia5 - Metodos y ayuda.py
@@ -1,1 +1,22 @@
 diccionario = {'clave1': 'agua', 'clave2': 34}
+a = diccionario.popitem()
+print(a)
+
+#ejercicio 1:
+textoek = ",:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#"
+ejemplo = textoek.lstrip(",:_#%") #remueve solo los caracteres de la izquierda?
+print(ejemplo)
+
+#ejercicio 2:
+frutas = ["mango", "banana", "cereza", "ciruela", "pomelo"]
+frutas.insert(3,"Naranja")
+print(frutas)
+
+#ejercicio 3:
+marcas_smartphones = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
+marcas_tv = {"Sony", "Philips", "Samsung", "LG"}
+conjuntos_aislados = marcas_smartphones.isdisjoint(marcas_tv)
+print(conjuntos_aislados)
+
+''' isdisjoint() ->  Return True if the set has no elements in common with other. 
+Sets are disjoint if and only if their intersection is the empty set.'''
---
+++ Dia 5/Functions.py
@@ -0,0 +1,27 @@
+def mi_funcion(nombre):
+    """ texto descriptivo para buenas
+    prácitcas, esta funcion se encarga de
+    imprimir un saludo en pantalla """
+    print("Hola " + nombre)
+
+
+mi_funcion("Raul")
+
+
+# Ejemplo 1:
+def bienvenida(nombre):
+    print(f"¡Bienvenido {nombre}!")
+
+
+nombre_persona = "Antonio"
+bienvenida(nombre_persona)
+
+# Ejemplo 2:
+
+
+def cuadrado(numero):
+    print(f"La potencia del numero es {numero**2}")
+
+
+un_numero = 4
+cuadrado(un_numero)
