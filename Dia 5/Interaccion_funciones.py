c8d0c7733fc30f23f1fb6a96674c289092b11a22
Raul Martos <raul@MacBook-Pro-de-Raul.local>
2025-03-26 13:57:46 -0600

    updating interaccion_funciones file just adding new excersice completed while studying other topcis, also added new blank file called argumentos_indefinidos

--- Dia 5/Interaccion_funciones.py
+++ Dia 5/Interaccion_funciones.py
@@ -25,7 +25,7 @@ def chequear_intento(lista, intento):
     else:
         print("Te salvaste de lavar los platos we")
 
-    print(f"Te ha tocado éste palitoek: {lista[intento - 1]}")
+    print(f"Te ha tocado éste palitoek: {lista[intento - 1]}\n")
 
 
 # Aquí todavía no se ejecuta nada, solo se crearon las funciones, abajo se invoca cada una en una variable
@@ -42,15 +42,15 @@ def lanzar_dados():
 
 
 def evaluar_jugada(lanzar1, lanzar2):
-    suma_dados = lanzar1, lanzar2
-    if lanzar1 + lanzar2 <= 6:
-        print(f"La suma de tus dados es {suma_dados}. Lamentable.")
-    elif 6 < lanzar1 + lanzar2 < 10:
-        print(f"La suma de tus dados es {suma_dados}. Tienes buenas chances.")
-    elif lanzar1 + lanzar2 >= 10:
-        print(f"La suma de tus dados es {suma_dados}. Parace una jugada ganadora.")
+    suma_dados = lanzar1 + lanzar2
+    if suma_dados <= 6:
+        resultado = f"La suma de tus dados es {suma_dados}. Lamentable."
+    elif 6 < suma_dados < 10:
+        resultado = f"La suma de tus dados es {suma_dados}. Tienes buenas chances."
+    elif suma_dados >= 10:
+        resultado = f"La suma de tus dados es {suma_dados}. Parace una jugada ganadora."
 
-    return suma_dados
+    return resultado
 
 
 dato1, dato2 = lanzar_dados()
@@ -58,8 +58,31 @@ suma = evaluar_jugada(dato1, dato2)
 print(suma)
 
 # Ejercicio 2
+lista_numeros = [1, 2, 7, 10]
+
+
+def reducir_lista(listadevolver):
+    sin_duplicados = list(set(listadevolver))
+    sin_duplicados.remove(max(sin_duplicados))
+    return sin_duplicados
+
+
+def promedio(listadev):
+    promedioek = 0
+    total = len(listadev)
+    for numero in listadev:
+        promedioek += numero
+    average = promedioek / total
+    return average
+
+
+lista_reducida = reducir_lista(lista_numeros)
+pro = promedio(lista_reducida)
+resultado = f"el promedio de la lista es {pro}"
 
 # Ejercicio 3
+
+
 
 
 
