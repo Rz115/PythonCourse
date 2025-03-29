315d84fbb0df69a505b478a1cf66d11cce5475b8
Raul Martos <raul@MacBook-Pro-de-Raul.local>
2025-03-29 14:13:53 -0600

    updating file argumentos indefinidos kwargs

--- Dia 5/Argumentos_indefinidos_kwargs.py
+++ Dia 5/Argumentos_indefinidos_kwargs.py
@@ -15,5 +15,23 @@ en la funcion prueba hasta abajo con sus respectivos asteriscos...'''
 valores = [10, 20, 30, 40, 50]
 valoresextra = {'x': 'uno', 'y': 'dos', 'z': 'tres'}
 
-prueba(15, 50, *valores, **valoresextra)
+# prueba(15, 50, *valores, **valoresextra)
+
+
+# Section test 1:
+def cantidad_atributos(**kwargs):
+    cantidad = len(kwargs)
+    return cantidad
+
+
+# Section test 2:
+def lista_atributos(**kwargs):
+    return list(kwargs.values())
+
+
+valoresextra = {'x': 'azul', 'y': 'rojo', 'z': 'verde', 'a': 24}
+resultado = lista_atributos(**valoresextra)
+# print(resultado)
+
+# Section test 3:
 
